6484ccb4bf860c91bc88d5fbc188b954a6df3f61
iambenbright <yankeybenneth@gmail.com>
2020-10-06 23:58:50 +0000

    feat: refactor initial app render

--- src/controller.ts
+++ src/controller.ts
@@ -3,12 +3,13 @@ import { View } from './view';
 
 export class Controller {
   constructor(public view: View, public model: Model) {
+    // initial render
+    this.view.render(this.model.getTodos());
     // bind addTodo
     this.view.attachAddTodo(this.model.addTodo);
+    // bind onTodosChanged
+    this.model.onTodosChanged(this.view.render);
   }
-
-  // initial app render
-  renderApp() {}
 
   bindRemoveTodo() {}
 
