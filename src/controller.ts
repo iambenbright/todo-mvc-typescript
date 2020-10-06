8b871d628d495b22caf4ea1ec11cbb03bb6d80ba
iambenbright <yankeybenneth@gmail.com>
2020-10-06 19:25:25 +0000

    fix: refactor addTodo and attachTodo bind

--- src/controller.ts
+++ src/controller.ts
@@ -2,12 +2,13 @@ import { Model } from './model';
 import { View } from './view';
 
 export class Controller {
-  constructor(public view: View, public model: Model) {}
+  constructor(public view: View, public model: Model) {
+    // bind addTodo
+    this.view.attachAddTodo(this.model.addTodo);
+  }
 
   // initial app render
   renderApp() {}
-
-  bindAddTodo() {}
 
   bindRemoveTodo() {}
 
