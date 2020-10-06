173af76d854d9946da01e1d636fa79424c71f26f
iambenbright <yankeybenneth@gmail.com>
2020-10-06 04:13:58 +0000

    feat: add view boilerplate

---
+++ src/view.ts
@@ -0,0 +1,21 @@
+export class View {
+  constructor(public app: HTMLDivElement) {}
+
+  render() {}
+
+  attachAddTodo() {}
+
+  attachRemoveTodo() {}
+
+  attachEditTodo() {}
+
+  attachToggleComplete() {}
+
+  private createForm() {}
+
+  private createFragment() {}
+
+  private createElement() {}
+
+  private getElement() {}
+}
