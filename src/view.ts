faecc831ac796f74786dcfebc9590d1b97740875
iambenbright <yankeybenneth@gmail.com>
2020-10-06 05:08:07 +0000

    feat: add create form logic

--- src/view.ts
+++ src/view.ts
@@ -1,5 +1,7 @@
 export class View {
-  constructor(public app: HTMLDivElement) {}
+  constructor(public app: HTMLDivElement) {
+    this.app.append(this.createForm());
+  }
 
   render() {}
 
@@ -11,7 +13,16 @@ export class View {
 
   attachToggleComplete() {}
 
-  private createForm() {}
+  private createForm(): DocumentFragment {
+    const html = `
+      <form>
+        <label for="text-input" hidden>Todo</label>
+        <input type="text" name="text-input" placeholder="what do you want to do today?" />
+        <button>Add Todo</button>
+      </form>
+    `;
+    return document.createRange().createContextualFragment(html);
+  }
 
   private createFragment() {}
 
