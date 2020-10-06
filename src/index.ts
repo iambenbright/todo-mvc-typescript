d0c36688444881d044a18e8cb1ba7979d1bbd33f
iambenbright <yankeybenneth@gmail.com>
2020-10-06 19:27:10 +0000

    fix: refactor view to model bind

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,4 @@ import { Controller } from './controller.js';
 const root = document.querySelector('#app') as HTMLDivElement;
 
 // bind view to model
-new Controller(new View(root), new Model('Ben'));
+new Controller(new View(root), new Model());
