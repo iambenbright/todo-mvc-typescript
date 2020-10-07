324710a8341f4ad31263d9fc52a406dd2c9b4c70
iambenbright <yankeybenneth@gmail.com>
2020-10-07 22:27:13 +0000

    refactor remove todo view and model bind

--- src/controller.ts
+++ src/controller.ts
@@ -9,11 +9,11 @@ export class Controller {
     this.view.attachAddTodo(this.model.addTodo);
     // bind onTodosChanged
     this.model.onTodosChanged(this.view.render);
+    // bind removeTodo
+    this.view.attachRemoveTodo(this.model.removeTodo);
+    // bind editTodo
+    // this.view.attachEditTodo(this.model.editTodo);
+    // bind toggleComplete
+    // this.view.attachToggleComplete(this.model.toggleComplete);
   }
-
-  bindRemoveTodo() {}
-
-  bindEditTodo() {}
-
-  bindToggleComplete() {}
 }
